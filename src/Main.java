c28081adc163e72f0add9f399f87c31771d6882d
BaekHorang <ymbaek@se.kaist.ac.kr>
2020-11-16 22:53:24 +0900

    GUI practice commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,35 @@
+import gui.LaunchWindow;
+import gui.SimEngineFrame;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+
+        LaunchWindow launchWindow = new LaunchWindow();
+
+        //SimEngineFrame simEngineFrame = new SimEngineFrame();
+        /*
+        JFrame simEngineFrame = new JFrame();
+        simEngineFrame.setTitle("Simulation Engine Frame");
+        simEngineFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        simEngineFrame.setResizable(false);
+        simEngineFrame.setSize(420, 420);
+        simEngineFrame.setVisible(true);
+
+        ImageIcon image = new ImageIcon("logo.png");
+        simEngineFrame.setIconImage(image.getImage());
+
+        simEngineFrame.getContentPane().setBackground(Color.DARK_GRAY);
+        simEngineFrame.getContentPane().setBackground(new Color(0,0,0));
+
+         */
+
     }
+
+
 }
