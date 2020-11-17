1eb3f6e5d27c9f526f542a4b184b4dedc695442b
Young-Min Baek <ymbaek@se.kaist.ac.kr>
2020-11-18 02:12:06 +0900

    GUI classes updated. - SimInputUI - SimStatusBarPanel - SeparatorPanel
    Basic structure of the SimEngine
    - SimEngine
    - ToySoS (for Example)

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,49 @@
 import gui.LaunchWindow;
 import gui.SimEngineFrame;
+import kr.ac.kaist.se.controller.sim.SimEngine;
+import kr.ac.kaist.se.view.SimInputUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.sql.Timestamp;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        System.out.println("[" + timestamp + "] Simulation engine is launched.");
 
-        try {
-//            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-            UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
-            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        // Launch a GUI for taking input files from a user
+        launchSimInputUI();
 
-        } catch (UnsupportedLookAndFeelException e) {
-            System.err.println("Error Message: " + e);
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        LaunchWindow launchWindow = new LaunchWindow();
+        initSimModel();
+        ToySoS toySoS = new ToySoS();
+
+
+
+        SimEngine simEngine = new SimEngine(toySoS);
+
+        timestamp = new Timestamp(System.currentTimeMillis());
+        //System.out.println("[" + timestamp + "] Simulation engine is terminated.");
+
+//        try {
+////            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+//            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+//            UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+//            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+//
+//        } catch (UnsupportedLookAndFeelException e) {
+//            System.err.println("Error Message: " + e);
+//            e.printStackTrace();
+//        } catch (IllegalAccessException e) {
+//            e.printStackTrace();
+//        } catch (InstantiationException e) {
+//            e.printStackTrace();
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        LaunchWindow launchWindow = new LaunchWindow();
 
         //SimEngineFrame simEngineFrame = new SimEngineFrame();
         /*
@@ -44,6 +61,37 @@ public class Main {
         simEngineFrame.getContentPane().setBackground(new Color(0,0,0));
 
          */
+
+    }
+
+    /**
+     * Method to launch a GUI for taking input files from a user.
+     * This method instantiates SimInputUI (simInputUI),
+     * which has menus/buttons for choosing input files.
+     */
+    private static void launchSimInputUI() {
+        // Set Look and Feel using the UIManager for Swing Objects
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        } catch (UnsupportedLookAndFeelException e) {
+            System.err.println("Error Message: " + e);
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        //SimInputUI simInputUI = new SimInputUI();
+        SwingUtilities.invokeLater(new SimInputUI());
+    }
+
+
+    /**
+     * Initialization of a simulation model
+     */
+    private static void initSimModel() {
 
     }
 
---
+++ src/case/toy/ToySoS.java
@@ -0,0 +1,20 @@
+import kr.ac.kaist.se.model.sos.SoS;
+import kr.ac.kaist.se.simdata.output.intermediate.RunResult;
+import kr.ac.kaist.se.simdata.output.intermediate.UpdateResult;
+
+public class ToySoS extends SoS {
+
+    public ToySoS(){
+
+    }
+
+    @Override
+    public RunResult run() {
+        return null;
+    }
+
+    @Override
+    public UpdateResult update(RunResult runResult) {
+        return null;
+    }
+}
