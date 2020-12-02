530b6e319d16ec48323ffc78d46fe019824f938a
BaekHorang <ymbaek@se.kaist.ac.kr>
2020-12-02 21:56:21 +0900

    Organizational structure - ToySoS - SoS - Organization

--- src/case/toy/ToySoS.java
+++ src/case/toy/ToySoS.java
@@ -1,4 +1,4 @@
-import kr.ac.kaist.se.model.sos.Organization;
+import kr.ac.kaist.se.model.sos.EnumOrgType;
 import kr.ac.kaist.se.model.sos.SoS;
 import kr.ac.kaist.se.simdata.output.intermediate.RunResult;
 import kr.ac.kaist.se.simdata.output.intermediate.UpdateResult;
@@ -14,16 +14,32 @@ public class ToySoS extends SoS {
     @Override
     protected void initSoSModel() {
         timestamp = new Timestamp(System.currentTimeMillis());
+        System.out.println("[" + timestamp + "]  ===================================================================");
         System.out.println("[" + timestamp + "] (ToySoS:initSoSModel) An SoS model is initialized.");
 
         ExampleOrgType org1 = new ExampleOrgType(this, "ORG01", "Organization 01");
+        org1.setOrgType(EnumOrgType.COLLABORATIVE);
         ExampleOrgType org1A = new ExampleOrgType(this, "ORG01A", "Organization 01-A");
         ExampleOrgType org1B = new ExampleOrgType(this, "ORG01B", "Organization 01-B");
         ExampleOrgType org1AA = new ExampleOrgType(this, "ORG01AA", "Organization 01-A-A");
 
-        ExampleCSType cs01a = new ExampleCSType(this, null, "CS01A-001", "Constituent 01-A 001");
-        ExampleCSType cs01b = new ExampleCSType(this, null, "CS01B-001", "Constituent 01-B 001");
-        ExampleCSType cs02a = new ExampleCSType(this, null, "CS02A-001", "Constituent 02-A 001");
+        ExampleOrgType org2 = new ExampleOrgType(this, "ORG02", "Organization 02");
+        org1.setOrgType(EnumOrgType.ACKNOWLEDGED);
+        ExampleOrgType org2A = new ExampleOrgType(this, "ORG02A", "Organization 02-A");
+
+
+        ExampleCSType csA = new ExampleCSType(this, null, "CS0A", "Constituent A");
+        ExampleCSType csB = new ExampleCSType(this, null, "CS0B", "Constituent B");
+        ExampleCSType csC = new ExampleCSType(this, null, "CS0C", "Constituent C");
+        ExampleCSType csD = new ExampleCSType(this, null, "CS0D", "Constituent D");
+        ExampleCSType csE = new ExampleCSType(this, null, "CS0D", "Constituent E");
+        ExampleCSType csF = new ExampleCSType(this, null, "CS0F", "Constituent F");
+        ExampleCSType csG = new ExampleCSType(this, null, "CS0G", "Constituent G");
+        ExampleCSType csH = new ExampleCSType(this, null, "CS0H", "Constituent H");
+
+        //independent CS
+        ExampleCSType csI = new ExampleCSType(this, null, "CS0I", "Constituent I");
+
 
 
         /* Case 1: Organizational structure -> CS added */
@@ -32,7 +48,33 @@ public class ToySoS extends SoS {
         org1.addSubOrg(org1B);
         org1A.addSubOrg(org1AA);
 
+        org1.addCS(csA, true);
+        org1.addCS(csB, true);
+        org1.addCS(csB, true); //duplicated
+        org1A.addCS(csC, true);
+        org1B.addCS(csD, true);
+        org1B.addCS(csE, true);
+        org1AA.addCS(csF, true);
+        org1AA.addCS(csG, true);
+
+        org2.addCS(csH, true);
+
         this.addOrg(org1);
+        this.addOrg(org2);
+
+        this.addCS(csI);
+//        this.removeOrg(org1);
+//        this.removeOrg(org1A);
+
+//        org2.addSubOrgDynamically(org2A);
+
+//        org1.addCS(csA);
+//        org1.addCS(csB);
+//        org1A.addCS(csC);
+//        org1B.addCS(csD);
+//        org1B.addCS(csE);
+//        org1AA.addCS(csF);
+//        org1AA.addCS(csG);
 
         /* Case 2: CS added -> Organizational structure */
 
